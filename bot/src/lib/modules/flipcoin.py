e42dc21ab1d40e15d18d6c76ecd6f138805d9766
Enclo <jelch2002@gmail.com>
2017-04-11 09:47:27 -0500

    removed mooshoes bot directory, back to only src now

--- bot/src/lib/modules/flipcoin.py
+++
@@ -1,9 +0,0 @@
-import random
-
-
-def main():
-    flip_ = random.randint(1, 2)
-    if flip_ == 1:
-        return [["fileKeep", "server/assets/Tails.png"]]
-    elif flip_ == 2:
-        return [["fileKeep", "server/assets/Heads.png"]]
