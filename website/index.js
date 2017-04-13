d117cb67f0b239e94caeba2128c978a9a1d8d40d
MooShoe <mooshoegaming@gmail.com>
2017-04-13 17:42:33 -0500

    Commiting before I try to implement OAuth.

--- website/index.js
+++ website/index.js
@@ -1,1 +1,17 @@
-console.log("Hello, World!");
+var express = require("express");
+var handlebars = require("express-handlebars");
+
+var app = express();
+app.engine("handlebars", handlebars());
+app.set("view engine", "handlebars");
+
+app.get("/", function(req, res) {
+  res.render("index", {
+    title: "Ubi the Discord Bot",
+    text: "Handlebars.js Success!"
+  });
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
