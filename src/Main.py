e6bcc910c90e6aa3988e096c97d2b25f75fa23c7
Will Loconto <CloudPlaysLP@iCloud.com>
2017-04-13 20:45:38 -0500

    Formatted Calculator.

--- src/Main.py
+++ src/Main.py
@@ -101,5 +101,5 @@ async def on_ready():
     print(client.user.id)
 
 client.loop.create_task(con.main_runtime())
-client.run('Mjk3NTg4ODg2MzMzNTU0Njkw.C9CWOg.IT_jZBg6KxHgoSnQYCFRWiTEhng')
+client.run('removed')
 # Always change token to removed when committing
