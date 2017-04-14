2276dd236acb17fd91d1a4cdf6f897f6fc80d096
Will Loconto <CloudPlaysLP@iCloud.com>
2017-04-14 15:06:42 -0500

    Continued work on Define.

--- src/lib/essentials/commandHandler.py
+++ src/lib/essentials/commandHandler.py
@@ -42,6 +42,8 @@ def handle(payload):
         return ["Single", src.lib.modules.comics.main()]
     elif payload.content.lower().startswith(";poll"):
         return ["Single", src.lib.modules.poll.main(payload)]
+    elif payload.content.lower().startswith(";define"):
+        return ["Single", src.lib.modules.define.main(payload)]
     elif payload.content.lower() == ";meme":
         return ["Single", src.lib.modules.memes.main()]
     elif payload.content.lower() == ";stats":
